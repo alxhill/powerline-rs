87d70d94464cf63f9b19902c2b99befca6c16a02
Alexander Hill <me@alxhill.com>
2024-06-13 00:09:38 -0400

    autocommit

--- src/modules.rs
+++ src/modules.rs
@@ -13,6 +13,8 @@ mod duration;
 mod python_env;
 mod spacer;
 mod time;
+mod rust_detector;
+mod jenv;
 
 pub use cmd::{Cmd, CmdScheme};
 pub use cwd::{Cwd, CwdScheme};
---
+++ src/modules/jenv.rs
@@ -0,0 +1,18 @@
+use std::marker::PhantomData;
+use crate::{Color, Powerline};
+use crate::modules::Module;
+
+pub struct Jenv<S> {
+    scheme: PhantomData<S>
+}
+
+pub trait JenvScheme {
+    const BG_COLOR: Color;
+    const FG_COLOR: Color;
+}
+
+impl<S: JenvScheme> Module for Jenv<S> {
+    fn append_segments(&mut self, powerline: &mut Powerline) {
+        
+    }
+}
