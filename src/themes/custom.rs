31a803c3a71987f4cea70a4c3383fceaacecd941
Alexander Hill <me@alxhill.com>
2024-06-25 15:26:33 +0100

    custom fully implemented

--- src/themes/custom.rs
+++ src/themes/custom.rs
@@ -121,6 +121,7 @@ impl CmdScheme for CustomTheme {
 }
 
 impl CwdScheme for CustomTheme {
+    // todo: figure out how to handle additional props
     fn path_bg_colors() -> Vec<Color> {
         vec![dark_grey()]
     }
@@ -136,8 +137,22 @@ impl ExitCodeScheme for CustomTheme {
     color_from_json!(exit_code_fg, exit_code, fg, default_fg);
 }
 
-// todo
-impl GitScheme for CustomTheme {}
+impl GitScheme for CustomTheme {
+    color_from_json!(git_remote_bg, git, remote_bg, default_bg);
+    color_from_json!(git_remote_fg, git, remote_fg, default_fg);
+    color_from_json!(git_staged_bg, git, staged_bg, default_bg);
+    color_from_json!(git_staged_fg, git, staged_fg, default_fg);
+    color_from_json!(git_notstaged_bg, git, notstaged_bg, default_bg);
+    color_from_json!(git_notstaged_fg, git, notstaged_fg, default_fg);
+    color_from_json!(git_untracked_bg, git, untracked_bg, default_bg);
+    color_from_json!(git_untracked_fg, git, untracked_fg, default_fg);
+    color_from_json!(git_conflicted_bg, git, conflicted_bg, default_bg);
+    color_from_json!(git_conflicted_fg, git, conflicted_fg, default_fg);
+    color_from_json!(git_repo_clean_bg, git, clean_bg, default_bg);
+    color_from_json!(git_repo_clean_fg, git, clean_fg, default_fg);
+    color_from_json!(git_repo_dirty_bg, git, dirty_bg, default_bg);
+    color_from_json!(git_repo_dirty_fg, git, dirty_fg, default_fg);
+}
 
 impl PythonEnvScheme for CustomTheme {
     color_from_json!(pyenv_fg, py, venv_fg, default_fg);
@@ -157,8 +172,18 @@ impl SpacerScheme for CustomTheme {
     color_from_json!(color_bg, spacer, bg, default_bg);
 }
 
-impl HostScheme for CustomTheme {}
+impl HostScheme for CustomTheme {
+    color_from_json!(hostname_bg, hostname, bg, default_bg);
+    color_from_json!(hostname_fg, hostname, fg, default_fg);
+}
 
-impl UserScheme for CustomTheme {}
+impl UserScheme for CustomTheme {
+    color_from_json!(username_root_bg, username, root_bg, default_bg);
+    color_from_json!(username_bg, username, bg, default_bg);
+    color_from_json!(username_fg, username, fg, default_fg);
+}
 
-impl TimeScheme for CustomTheme {}
+impl TimeScheme for CustomTheme {
+    color_from_json!(time_bg, time, bg, default_bg);
+    color_from_json!(time_fg, time, fg, default_fg);
+}
